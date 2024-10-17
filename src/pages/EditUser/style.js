c1077de37c5804b5db07494ca81269243796de83
Max S <max.santosdev@gmail.com>
2024-10-16 23:36:26 -0300

    style(src/pages/EditUser): adjusting layout

--- src/pages/EditUser/style.js
+++ src/pages/EditUser/style.js
@@ -4,19 +4,24 @@ import * as colors from '../../config/colors';
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
-  gap: 1.5rem;
-  margin-top: 2rem;
+  gap: 1rem;
+  margin-top: 1rem;
 
   input {
     width: 100%;
-    padding: 1rem;
+    padding: 0.6rem;
     margin-top: 0.5rem;
-    font-size: 1rem;
+    font-size: 0.8rem;
     border-radius: 0.5rem;
     border: 1px solid ${colors.color2};
   }
+
+  label {
+    font-size: 0.8rem;
+  }
+
   button {
-    height: 2.5rem;
+    height: 2rem;
     border-radius: 0.5rem;
     border: none;
     background: ${colors.color1};
